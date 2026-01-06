6bdd84e9407e5b313bc33a9fcf876ec158858a5a
Aradhya Sharma <aradhyasharmaa96@gmail.com>
2026-01-06 07:03:15 +0530

    rakumo redesign

---
+++ src/main/java/com/storage/rakumo/RakumoApplication.java
@@ -0,0 +1,13 @@
+package com.storage.rakumo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RakumoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RakumoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/storage/rakumo/RakumoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storage.rakumo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RakumoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
